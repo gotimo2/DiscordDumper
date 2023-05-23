c3adbe6cd59d498a68ce7e3d14ddc979c90845d7
gotimo2 <timost1402@hotmail.com>
2023-05-23 14:02:00 +0200

    basic discord.net setup

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Discord.WebSocket;
+
+namespace DiscordDumper
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        { 
+
+            var Client = new DiscordSocketClient();
+
+            var token = Environment.GetEnvironmentVariable("TOKEN");
+
+            await Client.LoginAsync(Discord.TokenType.Bot, token);
+
+            await Client.StartAsync();
+
+            
+
+            await Task.Delay(-1);
+
+        }
+    }
+}
